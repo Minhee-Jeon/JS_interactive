14d56ce88bb5eeda67826b6ab8c18e46835f1f38
minhee <jmh3699@naver.com>
2022-10-03 16:43:59 +0900

    [SCROLL-01] 활성시킬 scene 결정하기
    활성화할 scene 인덱스를 `cusScene`에 담도록 작업함

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 // 전역변수 사용을 피하기 위해 함수 안에서 함
 (() => {
+  let yOffset = 0; // window.pageYOffset(deprecated) -> window.scrollY 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 세션들의 스크롤 높이값의 합
+  let curScene = 0; // 현재 활성화되어 보고 있는 scene 인덱스(scroll-section)
+
   const sceneInfo = [
     {
       // 0
@@ -43,11 +47,30 @@
     // 각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[i].obj.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < curScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+        if (yOffset > prevScrollHeight + sceneInfo[curScene].scrollHeight) {
+            curScene++;
+        }
+        if (yOffset < prevScrollHeight) {
+            if (curScene === 0) return;
+            curScene--;
+        }
+        console.log(curScene);
     }
 
     window.addEventListener('resize', setLayout);
-    console.log(sceneInfo);
+    window.addEventListener('scroll', () => {
+        yOffset = window.scrollY;
+        // console.log(window.scrollY);
+        scrollLoop();
+    });
   }
 
   setLayout();
